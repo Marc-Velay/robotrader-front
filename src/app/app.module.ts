e20c97907ee5196b73dc1d92e650ea0fd30d8256
Marc-Velay <marc.velay@gmail.com>
2017-11-10 10:18:41 +0100

    finished demo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,26 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component'
+
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+   ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [ HeroService, MessageService ],
 })
 export class AppModule { }
