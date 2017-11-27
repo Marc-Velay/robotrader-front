9836d8a1049727caf5d7e172271b0fe9df3ddb11
Marc-Velay <marc.velay@gmail.com>
2017-11-27 22:35:19 +0100

    correct authentication

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { catchError, map, tap } from 'rxjs/operators';
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/do'
 
 import { User } from './users';
 import { MessageService } from './message.service';
@@ -20,20 +21,30 @@ export class AuthenticationService {
   login(username: string, password: string) {
         const body = { username: 'user', password: 'picklerick' };
         //return this.http.post('/api/authenticate', JSON.stringify({ username: username, password: password }))
-        return this.http.post('http://127.0.0.1:9200/api/get-token/', body).pipe(
-          tap(res => console.log(res))
-        );
+        return this.http.post('http://127.0.0.1:9200/api/get-token/', body)/*.pipe(
+          tap(res => res),
+          do( res => function(res) {
+            let user = res.json();
+            if (user && user.token) {
+                user.firstName = 'User';
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('currentUser', JSON.stringify(user));
+            }
 
-            /*.map((response: Response) => {
+            return user;
+          })
+        );*/
+
+            .map((response: Response) => {
                 // login successful if there's a jwt token in the response
-                let user = response.json();
+                let user = response;
                 if (user && user.token) {
                     // store user details and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify(user));
                 }
 
                 return user;
-            });*/
+            });
     }
  
     logout() {
