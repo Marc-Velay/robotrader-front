35a964cff4bc359c0f879fa20436bbf4fb818f41
Marc-Velay <marc.velay@gmail.com>
2017-12-19 18:32:32 +0100

    mowhen I wrote this code, only god and I understood this code... now only god does. Comments left!

---
+++ src/app/items/items.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Item } from '../item';
+import { ItemService } from '../items.service';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent implements OnInit {
+  items: Item[] = [];
+
+  constructor(
+    private itemService: ItemService
+  ) { }
+
+  ngOnInit() {
+    this.getItems();
+  }
+
+
+  getItems(): void {
+    this.itemService.getItems()
+      .subscribe(items => {
+          this.items = items;
+      });
+  }
+}
